3aba0a4b6fe9306cf7172de7cd1b878e649c7e4f
Ata Can <atacanymc@gmail.com>
2023-10-02 19:41:44 +0300

    my oop solution

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,41 @@
 package com.trendyol.shipment;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        if (products == null || products.isEmpty()) {   // There are no products in the basket
+            return ShipmentSize.SMALL;
+        }
+
+        // Size's count Map
+        Map<ShipmentSize, Long> shipmentSizeCountMap = products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        if (products.size() < 3) {  // If there are less than 3 products
+            return Collections.max(shipmentSizeCountMap.keySet());
+        }
+
+        if (Collections.max(shipmentSizeCountMap.values()) >= 3) {   // If there are 3 or more of the same shipment size
+            ShipmentSize commonSize = shipmentSizeCountMap.entrySet().stream()
+                    .filter(entry -> entry.getValue() >= 3)
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+
+            if (commonSize != null) {   // return upper size
+                return commonSize.upperSize();
+            }
+        }
+
+        // If none of the above conditions are met, return the largest shipment size.
+        return Collections.max(shipmentSizeCountMap.keySet());
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,14 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public ShipmentSize upperSize() {
+        int nextOrdinal = this.ordinal() + 1;
+        if (nextOrdinal < ShipmentSize.values().length) {
+            return ShipmentSize.values()[nextOrdinal];
+        } else {
+            return X_LARGE;
+        }
+    }
 }
